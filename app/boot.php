e18880da938c85b3d259e8ffc8cb75e55424696e
mikka <ezsolt@gmail.com>
2020-11-06 11:29:56 +0100

    bin

---
+++ app/boot.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * 
+ * zenshiatsu boot
+ *  
+ */
+
+
+echo '<pre>';
+
+$hello_world = [
+    'status' => 'OK',
+    'message' => 'hello world, I am zenshiatsu'
+];
+
+
+
+print_r($hello_world);
+
+
+
+echo '</pre>';
+
+// eof
